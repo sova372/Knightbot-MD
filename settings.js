6b725c7418ff04606a4e0052e41d6a8405a65613
sova372 <crazycacoon7@gmail.com>
2025-08-11 00:54:29 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Cacoon', // Your name
+  botOwner: 'sova372', // Your name
   ownerNumber: '254762804776', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
